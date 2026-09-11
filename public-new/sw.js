5e8a04d2fd6d98f94988a37abdc177171b2d4d6f
skb060968 <skb060968@gmail.com>
2026-09-11 22:29:29 +0530

    v55

--- public-new/sw.js
+++ public-new/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'cardgamesmp-app-';
-const CACHE_VERSION = 'v54';
+const CACHE_VERSION = 'v55';
 const CACHE_NAME = `${CACHE_PREFIX}${CACHE_VERSION}`;
 
 self.addEventListener('install', (event) => {
